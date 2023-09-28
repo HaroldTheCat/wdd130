84be2b7669a6ef95e374e23a93caabab191d60fc
HaroldTheCat <dougrohal@gmail.com>
2023-09-27 18:59:55 -0600

    Fixes and Program Maths developed

---
+++ Intro Prog/group_project_2.py
@@ -0,0 +1,15 @@
+#equasions 
+length_of_square_side=input("Insert length of one side of the square in question: ")
+area_of_square=(int(length_of_square_side)**2)
+width_rectangle=input("Insert the width of the retangle: ")
+length_rectangle=input("Insert the length of the rectangle: ")
+area_of_retangle=int(length_rectangle)*int(width_rectangle)
+circle_radius=input('Input your circles radius: ')
+area_of_circle=float(3.14)*float(circle_radius)**2
+print('5')
+
+
+#Printing information
+print(f"This is your square's Area: {area_of_square}")
+print(f"This is your rectangles Area: {area_of_retangle}")
+print(f"This is your circle's Area:{area_of_circle}")
