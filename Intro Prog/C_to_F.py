2b34c4aef5bc4667069cd6bd1d4efde5a83cc30a
HaroldTheCat <dougrohal@gmail.com>
2023-10-02 12:41:14 -0600

    Program New and  link Fix

---
+++ Intro Prog/C_to_F.py
@@ -0,0 +1,3 @@
+temp_celcius=float(input('What is the tempratue in the reasonable mesurment system of Celcius? : '))
+c_to_f=float(temp_celcius)* 9 / 5 +32
+print (f'This is your silly imperial Temprature :{c_to_f:.1f} degrees.')
